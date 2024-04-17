ef92cfd712e9443f72f3166ae1971dd94ca284b8
Satyam Kesharwani <153318865+stym01@users.noreply.github.com>
2024-04-17 21:01:16 +0530

    Add files via upload

---
+++ 1_watermelon.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    if(w>2){
+    if(w%2==0)
+    cout<<"Yes";
+    else
+    cout<<"No";}
+    else cout<<"No";
+    return 0;
+}
